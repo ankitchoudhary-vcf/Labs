5306fd2b7f70c8c564aa9c651147837506ac7be0
ankit <ac2000choudhary@gmail.com>
2020-12-25 09:27:55 +0530

    formated

--- oops with c++ lab/binaryOperatorOverloadingUsingFriendFunction.cpp
+++ oops with c++ lab/binaryOperatorOverloadingUsingFriendFunction.cpp
@@ -1,52 +1,42 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class A
-{
+class A {
 private:
-int a;
+  int a;
 
 public:
-void set_a();
-void get_a();
-friend A operator -(A);	// Friend function which takes an object of A and return an object of A type.
+  void set_a();
+  void get_a();
+  friend A operator-(A); // Friend function which takes an object of A and
+                         // return an object of A type.
 };
 
+// Definition of set_a() function
+void A ::set_a() { a = 10; }
 
-//Definition of set_a() function
-void A :: set_a()
-{
-a = 10;
+// Definition of get_a() function
+void A ::get_a() { cout << a << "\n"; }
+
+// Definition of overloaded unary minus operator - friend function
+A operator-(A ob) {
+  ob.a = -(ob.a);
+  return ob;
 }
 
+int main() {
+  A ob;
+  ob.set_a();
 
-//Definition of get_a() function
-void A :: get_a()
-{
-cout<< a <<"\n";
-}
+  cout << "The value of a is : ";
+  ob.get_a();
 
+  // Calling operator overloaded function - to negate the value
+  ob = -ob; // ob object is passed as an argument to the friend function and its
+            // negative version is returned.
 
-//Definition of overloaded unary minus operator - friend function
-A operator -(A ob)
-{
-ob.a = -(ob.a);
-return ob;
-}
-
-
-int main()
-{
-A ob;
-ob.set_a();
-
-cout<<"The value of a is : ";
-ob.get_a();
-
-//Calling operator overloaded function - to negate the value
-ob = -ob;	//ob object is passed as an argument to the friend function and its negative version is returned.
-
-cout<<"The value of a after calling operator overloading friend function - is : ";
-ob.get_a();
+  cout << "The value of a after calling operator overloading friend function - "
+          "is : ";
+  ob.get_a();
 }
