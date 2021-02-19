41188d36362cbfe7f646e07a1ac2d586089388b4
ankit <ac2000choudhary@gmail.com>
2021-02-19 15:26:06 +0530

    update

---
+++ test1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("size of A %d \n", sizeof('A'));
+    printf("Size of Char %d\n", sizeof(char));
+    printf("%d\n", sizeof(3.2));
+    printf("%d", sizeof(3.2f));
+}
