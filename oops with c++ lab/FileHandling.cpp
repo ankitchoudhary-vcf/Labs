4b79b1c3b7f25ec5dda45b9e4ebfcbf8c5a23009
ankit <ac2000choudhary@gmail.com>
2021-03-14 12:05:59 +0530

    update

--- oops with c++ lab/FileHandling.cpp
+++ oops with c++ lab/FileHandling.cpp
@@ -8,9 +8,21 @@ using namespace std;
 int main()
 {
     ofstream fOut;  //Create output stream object
-    fOut.open("filehandling.dat"); // Open the file if exits else first create that file and open that file in write only mode.
-    fOut<<"Hello";  // Write data in the file
+    fOut.open("filehandling.txt", ios::app); // Open the file if exits else first create that file and open that file in write only mode.
+    fOut<<"Hello\n";  // Write data in the file
     fOut.close();   // Close the file
+
+    ifstream fin;  //Create input stream object
+    fin.open("filehandling.txt"); // Open the file in read only mode
+    string line;
+    
+    while(fin)  // Reading data from the file
+    {
+        getline(fin, line);
+
+        cout<<line<<endl;
+    }
+    fin.close(); // Close file
     
     return 0;
 }
